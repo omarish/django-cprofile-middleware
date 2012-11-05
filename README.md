fa67618c90de5c33d9242eb243786a3b1e1538b2
Omar Bohsali <omar.bohsali@gmail.com>
2012-11-05 15:16:22 -0800

    Update README and middleware code.

--- README.md
+++ README.md
@@ -1,4 +1,38 @@
 django-cprofile-middleware
 ==========================
 
-Helpful django cprofiling middleware.
+This is a simple profiling middleware for Django applications. I wrote it because I got tired of printing "start" "stop" "stop 2" in my programs to find the bottlenecks.
+
+I found simple example on @dcramer's [slideshare](http://www.slideshare.net/zeeg/django-con-high-performance-django-presentation) and modified it to support sorting.
+
+## Installing
+
+Move ```profile.py``` to an application's middleware directory. If you're creating the middleware directory for the first time, be sure to add an ```__init__.py``` file to it. We've all forgotten to do that many times before ;).
+
+Then, add ```profile.ProfilerMiddleware``` to your ```MIDDLEWARE_CLASSES```. 
+
+For example:
+
+```
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'startup.do.work.FindProductMarketFitMiddleware',
+    ...
+    'app.middleware.profile.ProfilerMiddleware'
+)
+```
+
+## Running & Sorting Results
+
+Once you've installed it, add ```?prof``` to any URL to see the profiler's stats. For example to see profile stats for ```http://localhost:8000/foo/```, visit ```http://localhost:8000/foo/?prof```.
+
+You can also pass some options:
+
+**count:** The number of results you'd like to see. Default is 100.
+
+**sort:** The field you'd like to sort results by. Default is ```time```. For all the options you can pass, see the [docs for pstats](http://docs.python.org/2/library/profile.html#pstats.Stats.sort_stats).
+
+## Enjoy!
+
+Email me with any questions: [omar.bohsali@gmail.com](omar.bohsali@gmail.com).
---
+++ profile.py
@@ -0,0 +1,46 @@
+try:
+    import cProfile as profile
+except ImportError:
+    import profile
+import pstats
+from cStringIO import StringIO
+from django.conf import settings
+
+
+class ProfilerMiddleware(object):
+    """
+    Simple profile middleware to profile django views. To run it, add ?prof to
+    the URL like this:
+
+        http://localhost:8000/view/?prof
+
+    Optionally pass the following to modify the output:
+
+    ?sort => Sort the output by a given metric. Default is time.
+        See http://docs.python.org/2/library/profile.html#pstats.Stats.sort_stats
+        for all sort options.
+
+    ?count => The number of rows to display. Default is 100.
+
+    This is adapted from an example found here:
+    http://www.slideshare.net/zeeg/django-con-high-performance-django-presentation.
+    """
+    def can(self, request):
+        return settings.DEBUG and 'prof' in request.GET and \
+            request.user is not None and request.user.is_staff
+
+    def process_view(self, request, callback, callback_args, callback_kwargs):
+        if self.can(request):
+            self.profiler = profile.Profile()
+            args = (request,) + callback_args
+            return self.profiler.runcall(callback, *args, **callback_kwargs)
+
+    def process_response(self, request, response):
+        if self.can(request):
+            self.profiler.create_stats()
+            io = StringIO()
+            stats = pstats.Stats(self.profiler, stream=io)
+            stats.strip_dirs().sort_stats(request.GET.get('sort', 'time'))
+            stats.print_stats(int(request.GET.get('count', 100)))
+            response.content = '<pre>%s</pre>' % io.getvalue()
+        return response
