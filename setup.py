d5ce7791cce03edfe9c8c79e5be3fe8c433db00f
Omar Bohsali <omar.bohsali@gmail.com>
2013-09-01 14:27:04 -0700

    Remove extra comments in setup.py.

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 
 setup(
     name = 'django_cprofile_middleware',
-    packages = ['django_cprofile_middleware'], # this must be the same as the name above
+    packages = ['django_cprofile_middleware'],
     version = '0.1',
     description = 'Easily add cProfile profiling to django views.',
     author = 'Omar Bohsali',
     author_email = 'omar.bohsali@gmail.com',
-    url = 'https://github.com/omarish/django-cprofile-middleware/',   # use the URL to the github repo
-    # download_url = 'https://github.com/peterldowns/mypackage/tarball/0.1', # I'll explain this in a second
-    keywords = ['django','profiling','cProfile'], # arbitrary keywords
+    url = 'https://github.com/omarish/django-cprofile-middleware/',
+    download_url = 'https://github.com/omarish/django-cprofile-middleware/tarball/0.1',
+    keywords = ['django','profiling','cProfile'],
     classifiers = [],
 )
