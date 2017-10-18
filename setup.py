f07a75f6e1e65a26ccfc22579801ae30b1160abd
Omar Bohsali <omar.bohsali@gmail.com>
2017-10-18 20:06:50 +0000

    Release v1.0.

--- setup.py
+++ setup.py
@@ -4,12 +4,15 @@ setup(
     name = 'django-cprofile-middleware',
     packages = ['django_cprofile_middleware'],
     license = 'MIT',
-    version = '0.5',
+    version = '1.0',
     description = 'Easily add cProfile profiling to django views.',
+    long_description = 'See https://github.com/omarish/django-cprofile-middleware/',
     author = 'Omar Bohsali',
     author_email = 'omar.bohsali@gmail.com',
     url = 'https://github.com/omarish/django-cprofile-middleware/',
-    download_url = 'https://github.com/omarish/django-cprofile-middleware/tarball/0.4',
+    download_url = 'https://github.com/omarish/django-cprofile-middleware/tarball/v1.0',
     keywords = ['django','profiling','cProfile'],
-    classifiers = [],
+    classifiers = [
+        "Framework :: Django"
+    ],
 )
